da3bcffc5238079368f72adc4a78df1e73dc718c
1297345608@qq.com <1297345608@qq.com>
2020-01-19 23:18:58 +0800

    首页歌单分类

---
+++ src/api/topcatelist.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+
+export default {
+  fetchToplist () {
+    return Vue.axios.get('/toplist')
+  }
+}
--- src/components/parts/catelist.vue
+++ src/components/parts/catelist.vue
@@ -1,13 +1,26 @@
 <template>
-<view>1</view>
+  <view class="wrap-box"> </view>
 </template>
 
 <script>
+import topApi from '@/api/topcatelist'
 export default {
-
+  created () {
+    this.getTopList()
+  },
+  methods: {
+    getTopList () {
+      topApi
+        .fetchToplist()
+        .then(response => {
+          console.log(response.data)
+        })
+        .catch(error => {
+          console.error(error)
+        })
+    }
+  }
 }
 </script>
 
-<style lang="less" scoped>
-  
-</style>
+<style lang="less" scoped></style>
